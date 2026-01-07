a5a76e7a1231868d0857b1c2320f65d2e49a606e
kriteeka24 <thapakriteeka01@gmail.com>
2026-01-07 20:37:15 +0545

    modified operations

--- sum.py
+++ sum.py
@@ -1,8 +1,30 @@
-def add_numbers():
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
-    return a + b
+def number_inputs():
+    a=float(input("Enter first number: "))
+    b=float (input ("Enter second number: "))
+    return a,b
 
-result = add_numbers()
-print("Sum =", result)
+
+def add_numbers(a,b):
+    return a+b
+
+def multiply_numbers(a,b):
+    return a*b
+
+def divide_numbers(a,b):
+    return float(a/b)
+
+def subtract_numbers(a,b):
+    return a-b
+
+
+x,y=number_inputs()
+
+sum = add_numbers(x,y)
+difference=subtract_numbers(x,y)
+product=multiply_numbers(x,y)
+division=divide_numbers(x,y)
+print("Sum =", sum)
+print("Difference=",difference)
+print("Product=",product)
+print("Division=",division)
 
