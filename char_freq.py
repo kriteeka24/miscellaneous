a05287c24472f2a6d2c70308b28491c118f2b794
kriteeka24 <thapakriteeka01@gmail.com>
2026-01-30 23:44:52 +0545

    character frequency counter

---
+++ char_freq.py
@@ -0,0 +1,10 @@
+text = input("Enter a string: ").lower()
+
+freq = {}
+
+for ch in text:
+    if ch.isalpha():
+        freq[ch] = freq.get(ch, 0) + 1
+
+for ch, count in freq.items():
+    print(ch, ":", count)
